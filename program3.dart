e0fa1affbef2201f2dbf004c12bc93c9df5d8dd9
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-09-11 00:05:26 +0500

    program 3 done

---
+++ program3.dart
@@ -0,0 +1,14 @@
+void main(){
+  var list={
+    'name':'Abdul Jalil Khan',
+    'class':'Section A',
+    'roll':'123456',
+    'grade':'A',
+    'percentage':'75%'
+  };
+ 
+
+  print("List:$list");
+
+  
+}
