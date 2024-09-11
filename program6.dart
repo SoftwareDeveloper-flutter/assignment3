c41443b78f2126960efd5a3181645202a310cfbc
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-09-11 21:20:54 +0500

    program 6

---
+++ program6.dart
@@ -0,0 +1,7 @@
+void main(){
+  List<String> list_string=["One","two","three","four"];
+  List<String> new_list=List.of(list_string.reversed);
+    print("The original list is: $list_string");
+  print("The new list after reversed is: $new_list");
+  
+}
