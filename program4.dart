10482870291c19662181c8f7ca19e815f430a336
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-09-11 20:22:49 +0500

    program 4

---
+++ program4.dart
@@ -0,0 +1,6 @@
+void main(){
+  List<int> list_numbers=[5,9,33,3,1,6];
+  list_numbers.sort();
+  print("The smallest number of list is: ${list_numbers.first}");
+  print("The greatest number of list is: ${list_numbers.last}");
+}
